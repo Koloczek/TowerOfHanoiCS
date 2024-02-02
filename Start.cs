b37002239d4868fd63bad9bf72c52861fc6fa669
Koloczek <kacper.kolaczkowski@protonmail.com>
2024-02-02 21:27:31 +0100

    Some changes

--- Start.cs
+++ Start.cs
@@ -20,21 +20,15 @@ namespace ProjectGameCS
                 towers[i] = new Stack<int>();
             }
 
-            for (int i = numberOfDisks; i > 0; i--)
-            {
-                towers[0].Push(i);
-            }
-
             bool isRunning = true;
 
             while (isRunning)
             {
-                Console.CursorVisible = false;
                 Console.Clear();
-                Console.WriteLine("  Wieże Hanoi");
-                Console.WriteLine("  1. Graj");
-                Console.WriteLine("  2. Zasady");
-                Console.WriteLine("  3. Wyjdź");
+                Console.WriteLine("Wieże Hanoi");
+                Console.WriteLine("1. Graj");
+                Console.WriteLine("2. Zasady");
+                Console.WriteLine("3. Wyjdź");
                 Console.WriteLine();
 
                 string wyborEkranuString = Console.ReadLine();
@@ -45,6 +39,8 @@ namespace ProjectGameCS
                     switch (wyborEkranuInt)
                     {
                         case 1:
+                            ChooseNumberOfDisks();
+                            ResetGame();
                             Game();
                             break;
                         case 2:
@@ -63,103 +59,141 @@ namespace ProjectGameCS
                             Console.WriteLine("Podano nieprawidłową opcję menu.");
                             break;
                     }
-                }
-                else
-                {
-                    Console.WriteLine("Podano nieprawidłową opcję menu.");
-                }
-                if (isRunning)
-                {
-                    Console.WriteLine("Naciśnij dowolny klawisz, aby kontynuować...");
-                    Console.ReadKey();
+
+                    if (isRunning)
+                    {
+                        Console.WriteLine("Naciśnij dowolny klawisz, aby kontynuować...");
+                        Console.ReadKey();
+                    }
                 }
             }
-        }
 
-        static void Rules()
-        {
-            Console.WriteLine("teks");
-            Console.WriteLine();
-        }
-
-        static void Game()
-        {
-            ResetGame();
-
-            int? sourceTower = null;
-
-            while (!CheckWin())
+            static void Rules()
             {
-                PrintTowers();
-                Console.WriteLine("Użyj strzałek lewo/prawo do wyboru wieży, spacja do wyboru/przeniesienia dysku.");
+                Console.WriteLine("1. W każdym ruchu możesz przenieść tylko jeden dysk.");
+                Console.WriteLine("Dysk ten musi być górny na jednym ze słupków.");
+                Console.WriteLine("2. Dysk można położyć tylko na większym dysku lub na pustym słupku.");
+                Console.WriteLine("Nigdy nie wolno kłaść większego dysku na mniejszym.");
+                Console.WriteLine("3. Celem gry jest przeniesienie wszystkich dysków ze słupka startowego");
+                Console.WriteLine("na słupek końcowy (ostatni).");
+                Console.WriteLine("Każdy słupek może służyć jako tymczasowy magazyn dla dysków, przy czym");
+                Console.WriteLine("zawsze należy przestrzegać wyżej wspomnianych zasad.");
+                Console.WriteLine();
+            }
 
-                var key = Console.ReadKey(true).Key; 
+            static void Game()
+            {
+                ResetGame();
 
-                switch (key)
+                int? sourceTower = null;
+
+                while (!CheckWin())
                 {
-                    case ConsoleKey.LeftArrow:
-                        selectedTower = Math.Max(0, selectedTower - 1);
-                        break;
-                    case ConsoleKey.RightArrow:
-                        selectedTower = Math.Min(towers.Length - 1, selectedTower + 1);
-                        break;
-                    case ConsoleKey.Spacebar:
-                        if (sourceTower == null)
-                        {
-                            sourceTower = selectedTower;
-                        }
-                        else
-                        {
-                            if (!MoveDisk(sourceTower.Value, selectedTower))
+                    PrintTowers();
+                    Console.WriteLine("Użyj strzałek lewo/prawo do wyboru wieży, spacja do wyboru/przeniesienia dysku.");
+
+                    var key = Console.ReadKey(true).Key;
+
+                    switch (key)
+                    {
+                        case ConsoleKey.LeftArrow:
+                            selectedTower = Math.Max(0, selectedTower - 1);
+                            break;
+                        case ConsoleKey.RightArrow:
+                            selectedTower = Math.Min(towers.Length - 1, selectedTower + 1);
+                            break;
+                        case ConsoleKey.Spacebar:
+                            if (sourceTower == null)
                             {
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.WriteLine("Nie można przenieść większego dysku na mniejszy. Spróbuj ponownie.");
-                                Thread.Sleep(2000);
+                                sourceTower = selectedTower;
                             }
                             else
                             {
-                                moves++;
-                                PrintTowers(); 
-                                if (CheckWin())
+                                if (!MoveDisk(sourceTower.Value, selectedTower))
                                 {
-                                    Console.WriteLine("Gratulacje! Wygrałeś w {0} ruchach!", moves);
+                                    WriteLineInColor("Nie można przenieść większego dysku na mniejszy. Spróbuj ponownie.", ConsoleColor.Red);
                                     Thread.Sleep(2000);
-                                    return; 
                                 }
+                                else
+                                {
+                                    moves++;
+                                    PrintTowers();
+                                    if (CheckWin())
+                                    {
+                                        Console.WriteLine("Gratulacje! Wygrałeś w {0} ruchach!", moves);
+                                        Thread.Sleep(1000);
+                                        return;
+                                    }
+                                }
+                                sourceTower = null;
                             }
-                            sourceTower = null;
-                        }
-                        break;
+                            break;
 
+                    }
                 }
+
+                Console.WriteLine("Gratulacje! Wygrałeś w {0} ruchach!", moves);
             }
 
-            Console.WriteLine("Gratulacje! Wygrałeś w {0} ruchach!", moves);
-        }
-
-        static bool MoveDisk(int from, int to)
-        {
-            if (towers[from].Count == 0 || (towers[to].Count > 0 && towers[from].Peek() > towers[to].Peek()))
+            static void WriteLineInColor(string message, ConsoleColor color)
             {
-                return false;
+                Console.ForegroundColor = color;
+                Console.WriteLine(message);
+                Console.ResetColor();
             }
 
-            towers[to].Push(towers[from].Pop());
-            return true;
-        }
 
-
-        static void PrintTowers()
-        {
-            Console.Clear();
-            Console.WriteLine("Ruchy: " + moves);
-
-            int maxHeight = towers.Max(t => t.Count);
-            int baseWidth = numberOfDisks * 2 + 1;
-            int towerSpacing = 3;
-
-            for (int level = maxHeight - 1; level >= 0; level--)
+            static bool MoveDisk(int from, int to)
             {
+                if (towers[from].Count == 0 || (towers[to].Count > 0 && towers[from].Peek() > towers[to].Peek()))
+                {
+                    return false;
+                }
+
+                towers[to].Push(towers[from].Pop());
+                return true;
+            }
+
+
+            static void PrintTowers()
+            {
+                Console.Clear();
+                Console.WriteLine("Ruchy: " + moves);
+
+                int maxHeight = towers.Max(t => t.Count);
+                int baseWidth = numberOfDisks * 2 + 1;
+                int towerSpacing = 3;
+
+                for (int level = maxHeight - 1; level >= 0; level--)
+                {
+                    for (int towerIndex = 0; towerIndex < towers.Length; towerIndex++)
+                    {
+                        if (towerIndex == selectedTower)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                        }
+
+                        var towerList = towers[towerIndex].ToList();
+                        int diskSize = maxHeight - level - 1 < towerList.Count ? towerList[maxHeight - level - 1] : 0;
+
+                        PrintDisk(diskSize, baseWidth);
+
+                        if (towerIndex == selectedTower)
+                        {
+                            Console.ResetColor();
+                        }
+
+                        if (towerIndex < towers.Length - 1)
+                        {
+                            Console.Write(new string(' ', towerSpacing));
+                        }
+                    }
+                    Console.WriteLine();
+                }
+
+                string baseLine = new string('-', towers.Length * (baseWidth + towerSpacing) - towerSpacing);
+                Console.WriteLine(baseLine);
+
                 for (int towerIndex = 0; towerIndex < towers.Length; towerIndex++)
                 {
                     if (towerIndex == selectedTower)
@@ -167,14 +201,13 @@ namespace ProjectGameCS
                         Console.BackgroundColor = ConsoleColor.DarkGray;
                     }
 
-                    var towerList = towers[towerIndex].ToList(); 
-                    int diskSize = maxHeight - level - 1 < towerList.Count ? towerList[maxHeight - level - 1] : 0; 
-
-                    PrintDisk(diskSize, baseWidth);
+                    string label = "[T" + (towerIndex + 1) + "]";
+                    int labelPadding = (baseWidth - label.Length) / 2;
+                    Console.Write(new string(' ', labelPadding) + label + new string(' ', labelPadding));
 
                     if (towerIndex == selectedTower)
                     {
-                        Console.ResetColor(); 
+                        Console.ResetColor();
                     }
 
                     if (towerIndex < towers.Length - 1)
@@ -185,75 +218,52 @@ namespace ProjectGameCS
                 Console.WriteLine();
             }
 
-            string baseLine = new string('-', towers.Length * (baseWidth + towerSpacing) - towerSpacing);
-            Console.WriteLine(baseLine);
 
-            for (int towerIndex = 0; towerIndex < towers.Length; towerIndex++)
+            static void PrintDisk(int diskSize, int baseWidth)
             {
-                if (towerIndex == selectedTower)
+                int padding = (baseWidth - diskSize * 2) / 2;
+                string disk = new string('=', diskSize * 2);
+                Console.Write(new string(' ', padding) + disk + new string(' ', padding));
+            }
+
+            static bool CheckWin()
+            {
+                return towers[0].Count == 0 && towers[1].Count == 0 && towers[2].Count == numberOfDisks;
+            }
+
+            static void ResetGame()
+            {
+                towers = new Stack<int>[3];
+                for (int i = 0; i < towers.Length; i++)
                 {
-                    Console.BackgroundColor = ConsoleColor.DarkGray; 
+                    towers[i] = new Stack<int>();
                 }
 
-                string label = "[T" + (towerIndex + 1) + "]";
-                int labelPadding = (baseWidth - label.Length) / 2;
-                Console.Write(new string(' ', labelPadding) + label + new string(' ', labelPadding));
-
-                if (towerIndex == selectedTower)
+                for (int i = numberOfDisks; i > 0; i--)
                 {
-                    Console.ResetColor(); 
+                    towers[0].Push(i);
                 }
 
-                if (towerIndex < towers.Length - 1)
-                {
-                    Console.Write(new string(' ', towerSpacing));
+                moves = 0;
+            }
+
+            static void ChooseNumberOfDisks()
+            {
+                while(true) { 
+                    Console.WriteLine("Wybierz liczbę dysków (3-8):");
+                    string input = Console.ReadLine();
+                    if (int.TryParse(input, out int disks) && disks >= 3 && disks <= 8)
+                    {
+                        numberOfDisks = disks;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nieprawidłowa wartość, domyślna wartość to: 3");
+                    }
                 }
             }
-            Console.WriteLine();
+
         }
-
-
-        static void PrintDisk(int diskSize, int baseWidth)
-        {
-            int padding = (baseWidth - diskSize * 2) / 2;
-            string disk = new string('=', diskSize * 2);
-            Console.Write(new string(' ', padding) + disk + new string(' ', padding));
-        }
-
-        static bool CheckWin()
-        {
-            return towers[0].Count == 0 && towers[1].Count == 0 && towers[2].Count == numberOfDisks;
-        }
-
-        static void ResetGame()
-        {
-            towers = new Stack<int>[3];
-            for (int i = 0; i < towers.Length; i++)
-            {
-                towers[i].new Stack<int>();
-            }
-            for (int i = numberOfDisks; i > 0; i--)
-            {
-                towers[0].Push(i); 
-            }
-
-            moves = 0; 
-        }
-
-        static void ChooseNumberOfDisks()
-        {
-            Console.WriteLine("Wybierz liczbę dysków (3-8):");
-            string input = Console.ReadLine();
-            if (int.TryParse(input, out int disks) && disks >= 3 && disks <= 8)
-            {
-                numberOfDisks = disks;
-            }
-            else
-            {
-                Console.WriteLine("Nieprawidłowa wartość, domyślna wartość to: 3");
-                numberOfDisks = 3;
-            }
-        }
-
     }
 }
